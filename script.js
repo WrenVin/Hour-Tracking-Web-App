f9293069d6acf02398d65e9bad6113afa75bf448
WrenVin <wrenvin@gmail.com>
2023-11-15 20:50:58 -0600

    v0.1

--- script.js
+++ script.js
@@ -48,6 +48,7 @@ document.getElementById('clockIn').addEventListener('click', function () {
     const firstName = document.getElementById('firstName').value;
     const lastName = document.getElementById('lastName').value;
     updateEmployeeStatus(firstName, lastName, 'clocked in');
+    //clockInOut(firstName, lastName, 'clock-in');
     document.getElementById('firstName').value = '';
     document.getElementById('lastName').value = '';
     //addLogEntry('clocked in', firstName, lastName);
@@ -57,6 +58,7 @@ document.getElementById('clockOut').addEventListener('click', function () {
     const firstName = document.getElementById('firstName').value;
     const lastName = document.getElementById('lastName').value;
     updateEmployeeStatus(firstName, lastName, 'clocked out');
+    //clockInOut(firstName, lastName, 'clock-out');
     document.getElementById('firstName').value = '';
     document.getElementById('lastName').value = '';
     //addLogEntry('clocked out', firstName, lastName);
@@ -68,25 +70,53 @@ fetch('/api/readsheet')
     console.log(data); // You can see the data in the browser console
       // Now you can manipulate the DOM to display this data
       //console.log(data)
-    displayData(data);
+      //displayData(data);
+      displayCards(data);
   })
   .catch(error => console.error('Error:', error));
 
-function displayData(data) {
-  // Assuming data.values is an array of rows
-  const sheetData = data.values;
-  // Example: Display data in a table
-  const table = document.createElement('table');
-  sheetData.forEach(row => {
-    const tr = document.createElement('tr');
-    row.forEach(cell => {
-      const td = document.createElement('td');
-      td.textContent = cell;
-      tr.appendChild(td);
+function displayCards(data) {
+    // Get the container where cards will be added
+    const cardsContainer = document.querySelector('.employee-cards');
+
+    // Clear any existing content
+    cardsContainer.innerHTML = '';
+
+    // Iterate through each person in the data
+    data.forEach(person => {
+        console.log(person);
+        // Create card elements
+        const card = document.createElement('div');
+        card.className = 'card';
+
+        const name = document.createElement('h3');
+        name.textContent = `${person.firstName} ${person.lastName}`;
+
+        const status = document.createElement('p');
+        if (person.status === '1') {
+            card.classList.add('clocked-in');
+            status.innerHTML = `<span class="status">Clocked In</span>`;
+        }
+        else {
+            card.classList.remove('clocked-in');
+            status.innerHTML = `<span class="status">Clocked Out</span>`;
+        }
+        //status.innerHTML = `Status: <span class="status">${person.status}</span>`;
+
+        const hours = document.createElement('p');
+        hours.innerHTML = `Total Hours: <span class="hours">${person.totalHours}</span>`;
+
+        // Append elements to card
+        card.appendChild(name);
+        card.appendChild(status);
+        card.appendChild(hours);
+
+        // Append card to the container
+        cardsContainer.appendChild(card);
     });
-    table.appendChild(tr);
-  });
-  document.body.appendChild(table); // Append the table to the body or any other element
 }
+
+
+
 
 
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 require('dotenv').config();
+
 const cors = require('cors');
 //onst apiKey = process.env.API_KEY;
 const email = process.env.EMAIL;
@@ -10,6 +11,9 @@ const port = 3000;
 // Serve static files from a specific folder
 app.use(cors());
 app.use(express.static('.'));
+
+// Add this line to parse JSON request bodies
+app.use(express.json());
 
 app.listen(port, () => console.log(`Server listening on http://localhost:${port}`));
 const { google } = require('googleapis');
@@ -26,15 +30,29 @@ const client = new google.auth.JWT(
 const sheets = google.sheets({ version: 'v4', auth: client });
 app.get('/api/readsheet', async (req, res) => {
   try {
-    const range = 'Sheet1!A1:B2'; // Update with your range
+    const range = 'employeeData!A1:D4'; // Update with your range
     const response = await sheets.spreadsheets.values.get({
-      spreadsheetId: spreadsheetId, // Replace with your spreadsheet ID
+      spreadsheetId: spreadsheetId,
       range: range,
     });
-    //console.log(response.data.rows);
-    res.send(response.data);
+
+    const rows = response.data.values;
+    if (rows.length) {
+      // Extract headers
+      const headers = rows[0];
+      const data = rows.slice(1).map(row => {
+        let rowData = {};
+        headers.forEach((header, index) => {
+          rowData[header] = row[index];
+        });
+        return rowData;
+      });
+      res.send(data);
+    } else {
+      console.log('No data found.');
+      res.send([]);
+    }
   } catch (error) {
     res.status(500).send(error);
   }
 });
-
