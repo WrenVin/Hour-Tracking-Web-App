2187a707c523d7930b9c449cc950b41e3632b6dc
WrenVin <wrenvin@gmail.com>
2023-11-16 07:55:51 -0600

    Working clock cards

--- script.js
+++ script.js
@@ -21,49 +21,65 @@ function addLogEntry(action, firstName, lastName) {
 }
         
 function updateEmployeeStatus(firstName, lastName, action) {
+    //get sheet to update card
+    userFound = false;
     const cards = document.querySelectorAll('.card');
-    let userFound = false;
-
-    cards.forEach(card => {
-        const name = card.querySelector('h3').textContent;
-        if (name === `${firstName} ${lastName}`) {
-            userFound = true;
-            if (action === 'clocked in') {
-                card.classList.add('clocked-in');
-                addLogEntry('clocked in', firstName, lastName);
-            } else {
-                card.classList.remove('clocked-in');
-                addLogEntry('clocked out', firstName, lastName);
-            }
-        }
-    });
-
-    if (!userFound) {
-        addLogEntry('error', `User not found: ${firstName} ${lastName}`);
-    }
+    fetch('/api/readsheet')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data); // You can see the data in the browser console
+            // Now you can manipulate the DOM to display this data
+            //console.log(data)
+            //displayData(data);
+            data.forEach(person => {
+                if (person.firstName === firstName && person.lastName === lastName) {
+                    cards.forEach(card => {
+                        const name = card.querySelector('h3').textContent;
+                        if (name === `${firstName} ${lastName}`) {
+                            userFound = true;
+                            if (person.status === '1') {
+                                card.classList.add('clocked-in');
+                                card.querySelector('.status').textContent = 'Clocked In';
+                                addLogEntry('clocked in', firstName, lastName);
+                            }
+                            else {
+                                card.classList.remove('clocked-in');
+                                card.querySelector('.status').textContent = 'Clocked Out';
+                                addLogEntry('clocked out', firstName, lastName);
+                            }
+                        }
+                    });
+                }
+            });
+            if (!userFound) {
+                        addLogEntry('error', `User not found: ${firstName} ${lastName}`);
+                    }
+        })
+        
+        .catch(error => console.error('Error:', error));
 }
 
 
 document.getElementById('clockIn').addEventListener('click', function () {
     const firstName = document.getElementById('firstName').value;
     const lastName = document.getElementById('lastName').value;
-    updateEmployeeStatus(firstName, lastName, 'clocked in');
     //clockInOut(firstName, lastName, 'clock-in');
+    updateSheet(firstName, lastName, '1');
+    //updateEmployeeStatus(firstName, lastName, 'clocked in');
     document.getElementById('firstName').value = '';
     document.getElementById('lastName').value = '';
     //addLogEntry('clocked in', firstName, lastName);
-    updateSheet(firstName, lastName, '1');
 });
 
 document.getElementById('clockOut').addEventListener('click', function () {
     const firstName = document.getElementById('firstName').value;
     const lastName = document.getElementById('lastName').value;
-    updateEmployeeStatus(firstName, lastName, 'clocked out');
+    updateSheet(firstName, lastName, '0');
+    //updateEmployeeStatus(firstName, lastName, 'clocked out');
     //clockInOut(firstName, lastName, 'clock-out');
     document.getElementById('firstName').value = '';
     document.getElementById('lastName').value = '';
     //addLogEntry('clocked out', firstName, lastName);
-    updateSheet(firstName, lastName, '0');
 });
 
 fetch('/api/readsheet')
@@ -118,6 +134,7 @@ function displayCards(data) {
     });
 }
 
+
 function updateSheet(firstName, lastName, status) {
     // Using a relative URL
     const url = '/api/writesheet';
@@ -134,9 +151,9 @@ function updateSheet(firstName, lastName, status) {
         body: JSON.stringify(data),
     })
     .then(response => response.text())
-    .then(text => {
-        console.log('Sheet update response:', text);
-       //addLogEntry('update', firstName, lastName); // Example log
+        .then(text => {
+            console.log(firstName, lastName, status);
+            updateEmployeeStatus(firstName, lastName);
     })
     .catch(err => {
         console.error('Error updating sheet:', err);
